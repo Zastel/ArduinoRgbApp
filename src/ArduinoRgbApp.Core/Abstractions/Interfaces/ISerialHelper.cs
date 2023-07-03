9dac3fa353e74e05453c45b3217406740bf29405
Zastel <mrsheepdev@gmail.com>
2023-07-03 16:07:50 +0200

    Made the implementation of the serial helper better and populated interface

--- src/ArduinoRgbApp.Core/Abstractions/Interfaces/ISerialHelper.cs
+++ src/ArduinoRgbApp.Core/Abstractions/Interfaces/ISerialHelper.cs
@@ -2,5 +2,31 @@
 
 public interface ISerialHelper
 {
-    
+    /// <summary>
+    /// Connects the serial interface.
+    /// </summary>
+    public void Connect();
+
+    /// <summary>
+    /// Disconnects the serial interface.
+    /// </summary>
+    public void Disconnect();
+
+    /// <summary>
+    /// Sends the Rainbow mode message to the arduino.
+    /// </summary>
+    public void RainbowMode();
+
+    /// <summary>
+    /// Sends the Default mode message to the arduino.
+    /// </summary>
+    public void DefaultMode();
+
+    /// <summary>
+    /// Sends the static RGB color mode message with the color values to the arduino.
+    /// </summary>
+    /// <param name="red">0-255 value for the red color.</param>
+    /// <param name="green">0-255 value for the green color.</param>
+    /// <param name="blue">0-255 value for the blue color.</param>
+    public void ApplyStaticColor(int red, int green, int blue);
 }
--- src/ArduinoRgbApp.WPF/Helpers/Communication/SerialHelper.cs
+++ src/ArduinoRgbApp.WPF/Helpers/Communication/SerialHelper.cs
@@ -8,47 +8,75 @@ namespace ArduinoRgbApp.WPF.Helpers.Communication;
 
 public class SerialHelper : ISerialHelper
 {
+    /// <summary>
+    /// Indicates if the serial port is connected or not.
+    /// </summary>
+    public bool IsSerialConnected = false;
+    
     private readonly SerialPort _serialPort;
 
+    /// <summary>
+    /// Initializes the SerialHelper object.
+    /// </summary>
+    /// <param name="port">The name of the port to connect to.</param>
     public SerialHelper(string port)
     {
         _serialPort = new SerialPort(port, 115200);
     }
     
+    /// <inheritdoc/>
     public void Connect()
     {
         try
         {
             _serialPort.Open();
+            IsSerialConnected = true;
         }
         catch (Exception e)
         {
             Console.WriteLine(e);
-            MessageBox.Show("Please give a valid port number or check your connection");
+            MessageBox.Show("Please give a valid port number or check your connection.");
+        }
+    }
+    
+    /// <inheritdoc/>
+    public void Disconnect()
+    {
+        try
+        {
+            _serialPort.Close();
+            IsSerialConnected = false;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            MessageBox.Show("An error occured while disconnecting the serial port.");
         }
     }
 
-    public void Disconnect()
-    {
-        _serialPort.Close();
-    }
-
+    /// <inheritdoc/>
     public void RainbowMode()
     {
         SendMessage("rainbow");
     }
 
+    /// <inheritdoc/>
     public void DefaultMode()
     {
         SendMessage("default");
     }
 
+    /// <inheritdoc/>
     public void ApplyStaticColor(int red, int green, int blue)
     {
-        Debug.WriteLine($"RGB|{red.ToString("000")}|{green.ToString("000")}|{blue.ToString("000")}");
-        SendMessage($"RGB|{red.ToString("000")}|{green.ToString("000")}|{blue.ToString("000")}");
+        Debug.WriteLine($"RGB|{red:000}|{green:000}|{blue:000}");
+        SendMessage($"RGB|{red:000}|{green:000}|{blue:000}");
     }
 
+    /// <summary>
+    /// Sends a message via the connected serial port.
+    /// </summary>
+    /// <param name="message"></param>
     private void SendMessage(string message)
     {
         try
